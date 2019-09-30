c27beaac218e1506fea9cfef1ba9db84a1db5a8e
sh1ch <isogucchi-code@outlook.com>
2019-09-30 11:07:46 +0900

    feat: RegionCall の追加。
    Region Class は、空のクラスに Region ディレクティブを設定しています。

---
+++ Templates/ItemTemplates/Visual C#/RegionClass/RegionClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// $safeitemname$ クラスは、＊＊＊をサポートするためのクラスです。
+    /// </summary>
+    public class $safeitemname$
+    {
+        #region Fields
+        
+        #endregion
+        
+        #region Properties
+        
+        #endregion
+
+        #region Initializes
+
+        /// <summary>
+        /// <see cref="$safeitemname$"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        public $safeitemname$(){  }
+        
+        #endregion
+
+        #region Events
+        
+        #endregion
+
+        #region Public Methods
+        
+        #endregion
+        
+        #region Private Methods
+        
+        #endregion
+    }
+}
