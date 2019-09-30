55fff95ef26e1771c82622c75c6912a5e36eb789
sh1ch <isogucchi-code@outlook.com>
2019-09-30 12:02:46 +0900

    feat: Enum の追加。
    Enum は <summary> を持つのでコメントのテンプレート文の入力を省略できます。

---
+++ Templates/ItemTemplates/Visual C#/Enum/Enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// <see cref="$safeitemname$"/> 列挙型は、＊＊＊を示す識別子を表します。
+    /// </summary>
+    public enum $safeitemname$
+    {
+        
+    }
+
+}
