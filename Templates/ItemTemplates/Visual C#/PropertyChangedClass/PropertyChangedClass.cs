4ede162f197cdee9ae2f2acaf607e9c510540d10
sh1ch <isogucchi-code@outlook.com>
2019-09-30 11:43:34 +0900

    feat: PropertyChangedClass の追加。
    空の Region ディレクティブ＋INotifyPropertyChanged のインターフェースの継承をしたクラスです。

---
+++ Templates/ItemTemplates/Visual C#/PropertyChangedClass/PropertyChangedClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// <see cref="$safeitemname$"/> クラスは、＊＊＊をサポートするためのクラスです。
+    /// </summary>
+    public class $safeitemname$ : INotifyPropertyChanged
+    {
+        #region Fields
+        
+        #endregion
+
+        #region Properties
+        
+        #endregion
+
+        #region Initializes
+
+        /// <summary>
+        /// <see cref="$safeitemname$"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        public $safeitemname$() 
+        {
+        	
+        }
+        
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        #endregion
+
+        #region Public Methods
+        
+        #endregion
+
+        #region Private Methods
+        
+        #endregion
+    }
+}
