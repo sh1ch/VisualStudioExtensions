e3e43e49a2c06fa616b27ee67915e96547e85797
sh1ch <isogucchi-code@outlook.com>
2019-11-07 09:57:51 +0900

    feat: Interface のテンプレートを追加。

---
+++ Templates/ItemTemplates/Visual C#/Interface/ITemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// $safeitemname$ インターフェースは、値型・クラス型で実装する＊＊＊を目的とするメソッド・プロパティを定義します。
+    /// </summary>
+    public interface $safeitemname$
+    {
+        
+    }
+}
